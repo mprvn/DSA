6237982a571596e22baa173e795c12c184e0b911
praveen moolan <mln.prvn@gmail.com>
2022-11-12 00:18:27 +0530

    Anti Diagonal

--- DSASolutions/src/Arrays/AntiDiagonals.java
+++ DSASolutions/src/Arrays/AntiDiagonals.java
@@ -2,12 +2,53 @@ package src.Arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+/**
+ * Problem Description
+ * Give a N * N square matrix A, return an array of its anti-diagonals. Look at the example for more details.
+ * Problem Constraints
+ * 1<= N <= 1000
+ * 1<= A[i][j] <= 1e9
+ * Input Format
+ * Only argument is a 2D array A of size N * N.
+ * Output Format
+ * Return a 2D integer array of size (2 * N-1) * N, representing the anti-diagonals of input array A.
+ * The vacant spaces in the grid should be assigned to 0.
+ * Example Input
+ * Input 1:
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ * Input 2:
+ * 1 2
+ * 3 4
+ * Example Output
+ * Output 1:
+ * 1 0 0
+ * 2 4 0
+ * 3 5 7
+ * 6 8 0
+ * 9 0 0
+ * Output 2:
+ * 1 0
+ * 2 3
+ * 4 0
+ * Example Explanation
+ * For input 1:
+ * The first anti diagonal of the matrix is [1 ], the rest spaces shoud be filled with 0 making the row as [1, 0, 0].
+ * The second anti diagonal of the matrix is [2, 4 ], the rest spaces shoud be filled with 0 making the row as [2, 4, 0].
+ * The third anti diagonal of the matrix is [3, 5, 7 ], the rest spaces shoud be filled with 0 making the row as [3, 5, 7].
+ * The fourth anti diagonal of the matrix is [6, 8 ], the rest spaces shoud be filled with 0 making the row as [6, 8, 0].
+ * The fifth anti diagonal of the matrix is [9 ], the rest spaces shoud be filled with 0 making the row as [9, 0, 0].
+ * For input 2:
+ * The first anti diagonal of the matrix is [1 ], the rest spaces shoud be filled with 0 making the row as [1, 0, 0].
+ * The second anti diagonal of the matrix is [2, 4 ], the rest spaces shoud be filled with 0 making the row as [2, 4, 0].
+ * The third anti diagonal of the matrix is [3, 0, 0 ], the rest spaces shoud be filled with 0 making the row as [3, 0, 0].
+ * */
 public class AntiDiagonals {
 
     public ArrayList<ArrayList<Integer>> diagonal(ArrayList<ArrayList<Integer>> A) {
-        ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
-        int row= 0;
+        ArrayList<ArrayList<Integer>> output = new ArrayList<>();
+        int row;
         int col;
         for(int j=0; j< A.size(); j++){
             row = 0 ;col = j;
@@ -52,7 +93,7 @@ public class AntiDiagonals {
         Integer[] A =  {1,2,3};
         Integer[] B =  {4,5,6};
         Integer[] C =  {7,8,9};
-        ArrayList<ArrayList<Integer>> input = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
         input.add(new ArrayList<>(Arrays.asList(A)));
         input.add(new ArrayList<>(Arrays.asList(B)));
         input.add(new ArrayList<>(Arrays.asList(C)));
